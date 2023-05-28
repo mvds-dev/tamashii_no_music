720f2b028aa8f02ae18cae883b7d0872edc83292
Mateus Viana <min705555@gmail.com>
2023-05-28 13:33:14 -0300

    first commit base ideia

---
+++ main.py
@@ -0,0 +1,38 @@
+# Model :
+# Bussiness rule > Data Base in case a simple array, with a id, song name, artist field, and anime from the song;
+# Need make a CRUD to make things work, simple structure, and focus in change to POO;
+# Hexagonal Architecture test;
+music_list = [
+                {id:1, song:'ranbu no melody' artist:'SID',anime:'Bleach'},
+                {id:2, song:'Graduate' artist:'Kana Boom', anime : 'Yamada lv999' },
+                {id:3, song:'Monster', artist:'Yoasobi', anime:'Beastars'},
+                {id:4, song:'Idol', artist:'Yoasobi', anime:'Oshi no ko'},
+                {id:5, song:'Shukufuku', artist:'Yoasubi', anime:'Gundam : The witch from mercury'},
+                {id:6, song:'Uso' artist:'SID',anime:'FMA Brothehood'},
+                {id:7, song:'Good Morning World' artist:'BURNOUT SYNDROME',anime:'Dr.Stone'},
+                {id:8, song:'Blizzard' artist:'BURNOUT SYNDROME',anime:'Mashiro no Oto'},
+                {id:9, song:'Crossing Fields', artist:'LiSa', anime:'Swordart Online'},
+                {id:10, song:'Life' artist:'Rude-α',anime:'Dr.Stone'},
+                {id:11, song:'Where Do We Go?' artist:"Okamoto's",anime:'Dr.Stone NewWorld'},
+                {id:12, song:'Libera From Hell' artist:'Tarantula-Kasahara Yuri',anime:'Tengen Toppa Guren Lagann'},
+                {id:13, song:'Flashback' artist:'Mayavi',anime:'Koukkoku'},
+                {id:14, song:'Easy Brezzy' artist:'Chelmico',anime:'Eizouken'},
+                {id:15, song:'To the Beginning' artist:'Kalafina',anime:'Fate/Zero'},
+                {id:16, song:'KaiKai Kitan' artist:'Eve',anime:'Jujutsu Kaisen'},
+                {id:17, song:'Work', artist:'Millenium Parade and Sheena', anime:'Jigokuraku'},
+                {id:18, song:'Wasure Gataki', artist:'Huwie Ishizaki', anime:'Dr.Stone New World'},
+                {id:19, song:'Karei One Turn', artist:'Try sail', anime:'Isekai One Turn kill Neesan'},
+                {id:20, song:'Wasure Gataki', artist:'Huwie Ishizaki', anime:'Dr.Stone New World'},
+                {id:21, song:'Innocent Arrogance', artist:'BiSH',anime:'Tengoku Daimakyou'},
+                {id:22, song:'Destopy', artist:'Sizuk ft. Ayame',anime:'Hyōken no Majutsushi ga Sekai o Suberu'},
+                {id:23, song:'Love Crazy', artist:'Sumire Uesaka',anime:'Ijiranaide Nagatoro-San 2°Attack !'},
+                {id:24, song:'Who Ya Extend', artist:'Vivid Vice',anime:'Jujutsu Kaisen'},
+                {id:25, song:'Black Rover', artist:'Vickeblanca',anime:'Black Clover'},
+                {id:26, song:'Hydra', artist:'Myth&Roid',anime:'Overlord'},
+                {id:27, song:'Voracity', artist:'Myth&Roid',anime:'Overlord'},
+                {id:28, song:'Stynx Helix', artist:'Myth&Roid',anime:'Re:Zero', url:'https://music.youtube.com/watch?v=7uJxtwBFxxI'},
+                {id:29, song:'Memento', artist:'Nonoc',anime:'Re:Zero'},
+                {id:30, song:'Realize', artist:'Konomi Suzuki',anime:'ReZero'},
+              ]
+
+
